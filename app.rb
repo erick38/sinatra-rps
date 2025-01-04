02141ac4909e9af002a1e142615d794ebd7e3078
Sir Bedolla <69206592+erick38@users.noreply.github.com>
2025-01-04 01:56:09 +0000

    finished urls and functionality on every address

--- app.rb
+++ app.rb
@@ -2,8 +2,55 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+erb(:home)
+end
+
+get("/rock") do
+  moves = ['rock','paper','scissors']
+computer_move = moves.sample
+if computer_move == 'rock'
+  outcome_t='tied'
+elsif computer_move == 'paper'
+  outcome_t='lose'
+else computer_move == 'scissors'
+  outcome_t='win'
+end
+@outcome_c = "They played #{computer_move}"
+@outcome = "we #{outcome_t}"
+  erb(:rock)
+end
+
+get("/paper") do
+  moves = ['rock','paper','scissors']
+  computer_move = moves.sample
+  
+  @outcome_c = "They played #{computer_move}"
+  if computer_move == 'paper'
+    outcome_t='tied'
+  elsif computer_move == 'rock'
+    outcome_t='win'
+  else computer_move == 'scissors'
+    outcome_t='lose'
+  end
+  @outcome_c = "They played #{computer_move}"
+  @outcome = "we #{outcome_t}"
+  erb(:paper)
+
+end
+get("/scissors") do
+  moves = ['rock','paper','scissors']
+  computer_move = moves.sample
+  
+  @outcome_c = "They played #{computer_move}"
+
+  if computer_move == 'scissors'
+    outcome_t='tied'
+  elsif computer_move == 'rock'
+    outcome_t='lose'
+  else computer_move == 'paper'
+    outcome_t='win'
+  end
+  @outcome_c = "They played #{computer_move}"
+  @outcome = "we #{outcome_t}"
+  erb(:scissors)
 end
